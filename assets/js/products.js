851be53656721643b7e8c8627025cc08cc915d4c
AsktroNaut <votuan2791999@gmail.com>
2022-05-31 00:33:51 +0700

    q

--- assets/js/products.js
+++ assets/js/products.js
@@ -3,42 +3,48 @@ console.log(productContainer);
 
 
 
-fetch('http://159.223.67.62:1337/api/products?fbclid=IwAR19MiGOdxE_Ju-q0vj2g5JwrnofMjI8hZkAyqkg6Kg72XR1eOL24WAl7zc')
+fetch('http://159.223.67.62:1337/api/products?fbclid=IwAR03UF3mWvMEGpngRvnIB469ocO7eoJ4IYw_hnzFM1ufFDsEHECqULnJwVw')
   .then(response => response.json())
   .then(allProducts => {
         
-       
+       console.log(allProducts.data);
 
         allProducts.data.map(product => {
             const productProps = product.attributes
 
             let productItemHTML = document.createElement('div')
-            
+            productItemHTML.className = 'col-lg-3 col-md-4 col-sm-6'
+            productItemHTML.setAttribute('data-id', product.id)
             let productContentHtml = `
-                <div class="col-lg-3 col-md-4 col-sm-6" data-id="${product.id}">
+                
                     <div class="product-item">
                         <div class="product-img">
-                            <a href="single-product-variable.html?id=${product.id}">
+                            <a href="single-product.html?id=${product.id}">
                                 <img class="primary-img" src="${productProps.id}" alt="Product Images">
                                 <img class="secondary-img" src="${productProps.id}" alt="Product Images">
                             </a>
                             
                         </div>
                         <div class="product-content">
-                            <a class="product-name" href="single-product-variable.html?id=${product.id}">${productProps.title}</a>
+                            <a class="product-name" href="single-product.html?id=${product.id}">${productProps.title}</a>
                             <div class="price-box pb-1">
                                 <span class="new-price">$${productProps.price}</span>
                             </div>
                             
                         </div>
                     </div>
-                </div>
+                
+
+                
             `
             productItemHTML.innerHTML = productContentHtml
         console.log(productItemHTML);
-        productContainer.append(productItemHTML)
+        productContainer.appendChild(productItemHTML)
         }
         )
         }
             
     );
+
+
+                                        
