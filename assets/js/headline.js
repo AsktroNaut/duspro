a53ea79b94e819dd1e277170631d48240b13a549
AsktroNaut <votuan2791999@gmail.com>
2023-02-22 15:02:57 +0700

    Update headline.js

--- assets/js/headline.js
+++ assets/js/headline.js
@@ -1,5 +1,22 @@
 const headlineArea = document.querySelector('.breadcrumb-area')
 console.log(headlineArea);
+let screenWidth
 
-let screenWidth = screen.width
-console.log(screenWidth);
+const changeHeadline = () => {
+   
+    screenWidth = document.documentElement.clientWidth;
+    if (screenWidth < 500 ) {
+        headlineArea.setAttribute('style', 'background-image: url(assets/images/breadcrumb/bg/1-1-phone.jpg)')
+    } else {
+        headlineArea.setAttribute('style', 'background-image: url(assets/images/breadcrumb/bg/1-1-1919x388.jpg)')
+    }
+    console.log("doi size");
+    
+}
+
+changeHeadline()
+
+
+window.addEventListener('resize', changeHeadline ) 
+
+
